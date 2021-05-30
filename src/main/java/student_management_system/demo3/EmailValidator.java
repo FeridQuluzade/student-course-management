05816ea27ed962e7ee0f076a758f7a386afd6de9
Ferid07 <feridquluzade2002@gmail.com>
2021-05-30 18:45:54 +0400

    Initial commit

---
+++ src/main/java/student_management_system/demo3/EmailValidator.java
@@ -0,0 +1,17 @@
+package student_management_system.demo3;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Predicate<String> {
+    private static  final Predicate<String> IS_EMAIL_VALID=
+            Pattern.compile(
+                    "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                    Pattern.CASE_INSENSITIVE
+            ).asPredicate();
+
+    @Override
+    public boolean test(String s) {
+        return false;
+    }
+}
---
+++ src/main/java/student_management_system/demo3/datasource/DataSource.java
@@ -0,0 +1,20 @@
+package student_management_system.demo3.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
