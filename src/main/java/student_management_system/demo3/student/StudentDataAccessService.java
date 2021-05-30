ef57087eb2f87bec3c66cc376d83162a55900b44
Ferid07 <feridquluzade2002@gmail.com>
2021-05-30 19:00:04 +0400

    Task-1:create Student,StudentCourse,StudentAccessService

---
+++ src/main/java/student_management_system/demo3/student/StudentDataAccessService.java
@@ -0,0 +1,175 @@
+package student_management_system.demo3.student;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class StudentDataAccessService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    List<Student> selectAllStudents() {
+        String sql = "" +
+                "SELECT " +
+                " student_id, " +
+                " first_name, " +
+                " last_name, " +
+                " email, " +
+                " gender " +
+                "FROM student";
+
+        return jdbcTemplate.query(sql,mapStudentFomDb());
+    }
+    int insertStudent(UUID studentId, Student student) {
+        String sql = "" +
+                "INSERT INTO student (" +
+                " student_id, " +
+                " first_name, " +
+                " last_name, " +
+                " email, " +
+                " gender) " +
+                "VALUES (?, ?, ?, ?, ?::gender)";
+        return jdbcTemplate.update(
+                sql,
+                studentId,
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail(),
+                student.getGender().name().toUpperCase()
+        );
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    boolean isEmailTaken(String email) {
+        String sql = "" +
+                "SELECT EXISTS ( " +
+                " SELECT 1 " +
+                " FROM student " +
+                " WHERE email = ?" +
+                ")";
+        return jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{email},
+                (resultSet, i) -> resultSet.getBoolean(1)
+        );
+    }
+
+
+
+
+    private RowMapper<Student> mapStudentFomDb() {
+        return (resultSet, i) -> {
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+
+            String genderStr = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+            return new Student(
+                    studentId,
+                    firstName,
+                    lastName,
+                    email,
+                    gender
+            );
+        };
+    }
+    private RowMapper<StudentCourse> mapStudentCourseFromDb() {
+        return (resultSet, i) ->
+                new StudentCourse(
+                        UUID.fromString(resultSet.getString("student_id")),
+                        UUID.fromString(resultSet.getString("course_id")),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getString("department"),
+                        resultSet.getString("teacher_name"),
+                        resultSet.getDate("start_date").toLocalDate(),
+                        resultSet.getDate("end_date").toLocalDate(),
+                        Optional.ofNullable(resultSet.getString("grade"))
+                                .map(Integer::parseInt)
+                                .orElse(null)
+                );
+    }
+
+    List<StudentCourse> selectAllStudentCourses(UUID studentId) {
+        String sql = "" +
+                "SELECT " +
+                " student.student_id, " +
+                " course.course_id, " +
+                " course.name, " +
+                " course.description," +
+                " course.department," +
+                " course.teacher_name," +
+                " student_course.start_date, " +
+                " student_course.end_date, " +
+                " student_course.grade " +
+                "FROM student " +
+                "JOIN student_course USING (student_id) " +
+                "JOIN course         USING (course_id) " +
+                "WHERE student.student_id = ?";
+        return jdbcTemplate.query(
+                sql,
+                new Object[]{studentId},
+                mapStudentCourseFromDb()
+        );
+    }
+
+    int updateEmail(UUID studentId, String email) {
+        String sql = "" +
+                "UPDATE student " +
+                "SET email = ? " +
+                "WHERE student_id = ?";
+        return jdbcTemplate.update(sql, email, studentId);
+    }
+
+    int updateFirstName(UUID studentId, String firstName) {
+        String sql = "" +
+                "UPDATE student " +
+                "SET first_name = ? " +
+                "WHERE student_id = ?";
+        return jdbcTemplate.update(sql, firstName, studentId);
+    }
+
+    int updateLastName(UUID studentId, String lastName) {
+        String sql = "" +
+                "UPDATE student " +
+                "SET last_name = ? " +
+                "WHERE student_id = ?";
+        return jdbcTemplate.update(sql, lastName, studentId);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    boolean selectExistsEmail(UUID studentId, String email) {
+        String sql = "" +
+                "SELECT EXISTS ( " +
+                "   SELECT 1 " +
+                "   FROM student " +
+                "   WHERE student_id <> ? " +
+                "    AND email = ? " +
+                ")";
+        return jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{studentId, email},
+                (resultSet, columnIndex) -> resultSet.getBoolean(1)
+        );
+    }
+
+    int deleteStudentById(UUID studentId) {
+        String sql = "" +
+                "DELETE FROM student " +
+                "WHERE student_id = ?";
+        return jdbcTemplate.update(sql, studentId);
+    }
+
+}
---
+++ src/main/java/student_management_system/demo3/student/StudentService.java
@@ -0,0 +1,4 @@
+package student_management_system.demo3.student;
+
+public class StudentService {
+}
