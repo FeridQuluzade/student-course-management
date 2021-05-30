714f3abb683aaa29373c95e523d63a31575351d7
Ferid07 <feridquluzade2002@gmail.com>
2021-05-30 22:35:30 +0400

    Task-5: create StudentController and fix problem

--- src/main/java/student_management_system/demo3/student/StudentController.java
+++ src/main/java/student_management_system/demo3/student/StudentController.java
@@ -1,12 +1,48 @@
 package student_management_system.demo3.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/students")
 public class StudentController {
+    private final StudentService studentService;
 
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping(path = "{studentId}/courses")
+    public List<StudentCourse> getAllCoursesForStudent(
+            @PathVariable("studentId") UUID studentId) {
+        return studentService.getAllCoursesForStudent(studentId);
+    }
+
+    @PostMapping
+    public void addNewStudent(@RequestBody @Valid Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") UUID studentId,
+                              @RequestBody Student student) {
+        studentService.updateStudent(studentId, student);
+    }
+
+    @DeleteMapping("{studentId}")
+    public void deleteStudent(@PathVariable("studentId") UUID studentId) {
+        studentService.deleteStudent(studentId);
+    }
 
 }
