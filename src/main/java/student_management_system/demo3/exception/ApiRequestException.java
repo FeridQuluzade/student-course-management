95e99418a74cbb781a97c44e3cc73ce064b32588
FeridQuluzade <feridquluzade2002@gmail.com>
2021-07-06 17:54:05 +0400

    create getCourseById and getAllCourses func-ty for Course model

--- src/main/java/student_management_system/demo3/exception/ApiRequestException.java
+++ src/main/java/student_management_system/demo3/exception/ApiRequestException.java
@@ -1,5 +1,7 @@
 package student_management_system.demo3.exception;
 
+import java.util.function.Supplier;
+
 public class ApiRequestException extends RuntimeException {
     public ApiRequestException(String message) {
         super(message);
@@ -8,6 +10,7 @@ public class ApiRequestException extends RuntimeException {
     public ApiRequestException(String message, Throwable cause) {
         super(message, cause);
     }
+
 
 
 }
