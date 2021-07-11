87721fc920304238f6c1a94c68df3acf1b172f55
FeridQuluzade <feridquluzade2002@gmail.com>
2021-07-11 09:41:29 +0400

    Task-18: create ErrorHandler func-ty for Student model

---
+++ src/main/java/student_management_system/demo3/model/ErrorDto.java
@@ -0,0 +1,27 @@
+package student_management_system.demo3.model;
+
+public class ErrorDto {
+    private String code;
+    private String  message;
+
+    public ErrorDto(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
